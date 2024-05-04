15552d8416e3cfb182dffa79f1707763042d775a
Philip Johnson <p.zhao.johnson@gmail.com>
2024-05-04 11:03:49 -0400

    Set up repository to be an npm package

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    globalObject: 'this',
+    library: {
+      name: 'webView',
+      type: 'umd',
+    },
+    clean: true,
+  },
+};
